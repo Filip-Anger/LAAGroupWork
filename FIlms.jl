36ef4fc4c120ae27fa4fc4548ea71b489aa0f8de
FilipAnger <filip.anger@gmail.com>
2026-01-08 15:43:53 +0100

    Multiple normalizing options

--- FIlms.jl
+++ FIlms.jl
@@ -6,15 +6,21 @@ rating_matrix = [7.0	8	9	8	10	8	8	9;
                 7	8	10	9	7	9	5	6;
                 8	8	10	9	7	7	6	8;
                 9	7	8	8	9	9	9	7]
+display(rating_matrix)
 # Missing Chris' entries
+chris_half = [8.0, 8,0,0,7,0,9,0]
+filled_entries = [1, 1, 0, 0, 1, 0, 1, 0]
+# 0 are for the missing values
 # predicting
 
 # Shift by the average to get biggest and most informative vectors
-# '.-' is elementwise operation in Jullia 
+# '.' is elementwise operation in Jullia 
 average_of_matrix = round(sum(rating_matrix) / length(rating_matrix); digits=2)
 rating_matrix .-= average_of_matrix
-display(rating_matrix)
+chris_half .-= average_of_matrix
+chris_half .*= filled_entries
 
+# Test
 for i in 1:4
     for j in (i+1):5
         d = dot(rating_matrix[i, :], rating_matrix[j, :])
@@ -23,3 +29,15 @@ for i in 1:4
         println(names_in_order[i] , " vs " , names_in_order[j] , ": " , ang)
     end
 end
+
+# Calculate anlge Criss vs Everyone else, pick the smallest anlge
+
+for i in 1:5
+    d = dot(rating_matrix[i, :], chris_half)
+    n = norm(rating_matrix[i, :]) * norm(chris_half)
+    ang = acos(d/n) * (180/pi)
+    println(names_in_order[i] , " vs Chris: " , ang)
+end
+
+# Filip and Cris have the smallest angle between them
+# We will 
