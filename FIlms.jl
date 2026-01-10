a980d6358eefdfb9df558ae2a314819dc02cdcc7
FilipAnger <filip.anger@gmail.com>
2026-01-10 10:32:04 +0100

    Films - Structure

--- FIlms.jl
+++ FIlms.jl
@@ -1,48 +1,75 @@
 using LinearAlgebra
-films_in_order = ["Squid game 1st series", "Jurassic Park 1",	"Kung Fu Panda 1",	"Inside Out 1",	"Oppenheimer", "Breaking Bad 1st series", "Hunger Games 1",	"Shrek 1"]
-names_in_order = ["Demir", "Sumana", "Gia Hy Mai", "Zhiyong He", "Filip"]
-rating_matrix = [7	8	10	8	9	8	8	9;
-9	8	7	6	9	6	6	6;
-7	8	7	5	10	9	9	6;
-8	8	7	6	10	9	7	8;
-10	9	10	10	9	9	10	8]
-rating_of_4_films = rating_matrix[:,1:4]
-display(rating_matrix)
-# Missing Chris' entries
-chris_half = transpose([8.0, 8, 7, 9])
-# 0 are for the missing values
-# predicting
+# Name 	                    Demir	Sumana  Lahiri 	Gia-Hy-Mai	Zhiyong-He	Filip
+# Squid game 1st series
+# Jurassic Park 1
+# Kung Fu Panda 1
+# Inside Out 1
+# Oppenheimer
+# Breaking Bad 1st series
+# Hunger Games 1
+# Shrek 1
 
-# Shift by the average to get biggest and most informative vectors
-# '.' is elementwise operation in Jullia 
+# A)
+ratings_matrix = [  7	9	7	8	10;
+                    8	8	8	8	9;
+                    10	7	7	7	10;
+                    8	6	5	6	10;
+                    9	9	10	10	9;
+                    8	6	9	9	9;
+                    8	6	9	7	10;
+                    9	6	6	8	8;  ]
 
-# LAA way: every row - sum/len of that row
-# sum of that rows in a vector A * e
-# substarct the average from every entry of the matrix
-av_per_row = rating_of_4_films * ones(4) / 4
-rating_of_4_films -= av_per_row * ones(1, 4)
 
-check = rating_of_4_films * ones(4) / 4
-# Average is 0
+# B
+ratings_half = ratings_matrix[1:4, :]
+ratings_half_origin = ratings_half - (ones(4) * (transpose(ones(4)) * ratings_half) / 4)
+check = transpose(ones(4)) * ratings_half_origin 
 
-chris_half * ones(4) / 4
-chris_half -= (chris_half * ones(4) / 4) *  ones(1,4)
+half_demir_origin = ratings_half_origin[:, 1]
+half_others_origin = ratings_half_origin[:,2:5]
 
-display(rating_of_4_films)
-# Calculate anlge Criss vs Everyone else, pick the smallest anlge
+# C
 
-# VERY PRIMITIVE WAY: WIGHTED ANGLES
-angles = zeros(5)
-for i in 1:5
-    d = dot(rating_of_4_films[i, :], chris_half)
-    n = norm(rating_of_4_films[i, :]) * norm(chris_half)
+
+# D
+angles = zeros(4)
+for i in 1:4
+    d = dot(half_others_origin[:, i], half_demir_origin)
+    n = norm(half_others_origin[:, i]) * norm(half_demir_origin)
     angles[i] = acos(d/n)
 end
 
+# Angle of Sumana and other of us - acos(0) = 1
+# Normalize, sum = 1
 weights = angles ./ sum(angles)
 
-chris_rest = transpose(weights) * rating_matrix[:,5:8]
-# 9.4276  8.2068  7.96232  7.39552
-# Criss has these angles with others in V^4
-# Find a vector in V^8 which has the same angles with all vectors in V^8
 
+demir_actual_first = ratings_matrix[1:4,1]
+demir_predicited_first = Int.(round.(ratings_matrix[1:4, 2:5] * weights, digits = 0))
+mean_abs_error_training = norm(demir_actual_first .- demir_predicited_first, 1) / 4
+
+demir_actual_second = ratings_matrix[5:8,1]
+demir_predicited_second = Int.(round.(ratings_matrix[5:8, 2:5] * weights))
+mean_abs_error_test = norm(demir_actual_second .- demir_predicited_second, 1) / 4
+
+
+
+# TSVD
+# PLAN: 
+ratings_matrix = [  7	9	7	8	10;
+                    8	8	8	8	9;
+                    10	7	7	7	10;
+                    8	6	5	6	10;
+                    9	9	10	10	9;
+                    8	6	9	9	9;
+                    8	6	9	7	10;
+                    9	6	6	8	8;  ]
+
+ones(5, 8)
+
+# normalize ratings around 0 DONE
+
+# Make SVD of that 
+
+rating_matrix
+# Do truncated TSVD with k = 4
